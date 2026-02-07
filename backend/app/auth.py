e3c8ab2156fefb10476d6de48ca78d2cc0f7f493
lorène <lorene.ameyikpo@gmail.com>
2026-02-07 22:03:12 +0000

    remodification de main et auth

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,13 +1,19 @@
 #logique de hachage  et securité jwt
+from fastapi import Depends
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from app.auth import get_current_user
+import models, schema, auth
 
 SECRET_KEY = "groupe3chatbot" 
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def me(current_user: models.User = Depends(get_current_user)):
+    return current_user
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -21,3 +27,13 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+    try:
+        payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
+        username = payload.get("sub")
+        user = db.query(models.User).filter(models.User.username == username).first()
+        if user is None:
+            raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token invalide")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,35 +1,25 @@
 #point d'entree de l'application 
 
 from fastapi import FastAPI, Depends, HTTPException, status
-from .Routes import chat, users
+from app.Routes import chat, users
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from jose import jwt, JWTError
-import models
-import schema
-import auth
-from database import engine, get_db
+import models, schema, auth
+from app.database import engine, get_db
+from app.auth import get_current_user 
+
+models.Base.metadata.create_all(bind=engine)
   # Le point signifie "dans le dossier actuel"
 
 app = FastAPI(title = "Chatbot API")
 
 app.include_router(chat.router)
-app.include_router(users.router)
+app.include_router(users.router) 
 @app.get("/")
 def read_root():
     return {"status": "Online"}
-
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-import models
-import schema
-import auth
-from database import engine, get_db
-
-models.Base.metadata.create_all(bind=engine)
-
-
+ 
 
 # les trucs pour le  React
 app.add_middleware(
@@ -77,15 +67,6 @@ def login(user: schema.UserLogin, db: Session = Depends(get_db)):
     return {"access_token": access_token, "token_type": "bearer"}
 
 @app.get("/me", response_model=schema.UserResponse)
-def get_current_user(token: str, db: Session = Depends(get_db)):
+def me(current_user: models.User = Depends(get_current_user)):
+    return current_user
     
-    from jose import jwt
-    try:
-        payload = jwt.decode(token, auth.SECRET_KEY, algorithms=[auth.ALGORITHM])
-        username = payload.get("sub")
-        user = db.query(models.User).filter(models.User.username == username).first()
-        if user is None:
-            raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
-        return user
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Token invalide")
